1295634055f5d0072070435ded3a87e5d16435e1
Dobromir Kovachev <dobromir.mail@gmail.com>
2017-07-21 08:16:01 +0300

    working on authentication and redirecting system

--- client/main.js
+++ client/main.js
@@ -3,7 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
-
+import { Tracker } from 'meteor/tracker';
 
 // Pages
 import Signup from '../imports/ui/Signup';
@@ -15,9 +15,22 @@ import NotFound from '../imports/ui/NotFound';
 import Login from '../imports/ui/Login';
 
 // browserHistory
-window.browserHistory = browserHistory;
+// window.browserHistory = browserHistory;
 
+// Tracking user authentication
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId();
+  const pathname = browserHistory.getCurrentLocation().pathname;
+  const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
+  const isAuthenticatedPage = authenticatedPages.includes(pathname);
 
+  // if on signup or loging page and user is logged in redirect
+  
+});
+
+// Pages which can and can't be loaded regarding authentication status (if user is loged in or not)
+const unauthenticatedPages = ['/', '/signup', '/login'];
+const authenticatedPages = ['/profilesettings', '/approom', '/accountmanager'];
 
 // Routing
 const routes = (
